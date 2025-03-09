8ed6bcf2de268da05d49f0e7570acf1df6716d36
prialgon <alpri.gonzalez@gmail.com>
2025-03-09 12:29:53 +0100

    práctica terminada

--- src/Lab2_6_CV.py
+++ src/Lab2_6_CV.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def cross_validation(model, X, y, nFolds):
     """
     Perform cross-validation on a given machine learning model to evaluate its performance.
@@ -45,25 +48,27 @@ def cross_validation(model, X, y, nFolds):
         nFolds = X.shape[0]
 
     # TODO: Calculate fold_size based on the number of folds
-    fold_size = None
+    fold_size = X.shape[0]//nFolds
 
     # TODO: Initialize a list to store the accuracy values of the model for each fold
     accuracy_scores = []
 
     for i in range(nFolds):
         # TODO: Generate indices of samples for the validation set for the fold
-        valid_indices = None
+        valid_indices = np.arange(i*fold_size, (i+1)*fold_size)
 
         # TODO: Generate indices of samples for the training set for the fold
-        train_indices = None
+        train_indices = np.setdiff1d(np.arange(0, X.shape[0]), valid_indices)
 
         # TODO: Split the dataset into training and validation
-        X_train, X_valid = None, None
-        y_train, y_valid = None, None
+        X_train, X_valid = X[train_indices], X[valid_indices]
+        y_train, y_valid = y[train_indices], y[valid_indices]
 
         # TODO: Train the model with the training set
+        model.fit(X_train, y_train)
 
         # TODO: Calculate the accuracy of the model with the validation set and store it in accuracy_scores
+        accuracy_scores.append(model.score(X_valid, y_valid))
 
     # TODO: Return the mean and standard deviation of the accuracy_scores
-    return None, None
+    return np.mean(accuracy_scores), np.std(accuracy_scores)
